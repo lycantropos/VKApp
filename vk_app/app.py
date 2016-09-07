370e19d167a983366d9f0f7c0ac2aa55232fcf4a
ace <azatibrakov@gmail.com>
2016-09-07 17:47:16 +0700

    Replacing of VK script for getting items from 'config' to 'app' module

--- vk_app/app.py
+++ vk_app/app.py
@@ -2,8 +2,6 @@ import json
 
 from vk import API, Session, AuthSession
 
-from vk_app.config import VK_SCRIPT_GET_ALL
-
 
 class App:
     def __init__(self, app_id='', user_login='', user_password='', scope='', access_token='',
@@ -71,3 +69,18 @@ class App:
             count = code_res['count']
             if offset >= count:
                 return items
+
+
+VK_SCRIPT_GET_ALL = """var params = {2};
+var max_count = params.count, init_offset = params.offset, key = "{1}", offset_count = init_offset;
+var vk_api_req = API.{0}(params), c = vk_api_req.count, items_count = vk_api_req[key], i = 1;
+
+while (i < 25 && offset_count + max_count <= c) {{
+    offset_count = i * max_count + init_offset;
+    params.offset = offset_count;
+    items_count = items_count + API.{0}(params)[key];
+    i = i + 1;
+}}
+
+return {{count: c, items: items_count, offset: offset_count + max_count, max_c: max_count}};
+"""
