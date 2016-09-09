aa06311134e5f20526e996c5989dc24b9b03a3ae
ace <azatibrakov@gmail.com>
2016-09-09 14:13:07 +0700

    Working with 'datetime' objects instead of 'str'

--- vk_app/utils/time.py
+++ vk_app/utils/time.py
@@ -1,11 +1,6 @@
 import threading
 from datetime import datetime
 
-# hyphen was chosen because its MySQL default DATE separator
-DATE_SEP = '-'
-DATE_ORDER = ['%Y', '%m', '%d']
-DATE_FORMAT = DATE_SEP.join(DATE_ORDER)
-
 
 def make_periodic(delay: int):
     """Decorator with parameter for making functions periodically launched
@@ -27,11 +22,6 @@ def make_periodic(delay: int):
     return launch_periodically
 
 
-def get_date_from_millis(millis: int) -> str:
-    date = datetime.fromtimestamp(millis).strftime(DATE_FORMAT)
-    return date
-
-
-def get_year_month_date(date: str, sep='.') -> str:
-    year_month_date = sep.join(date.split(DATE_SEP)[:-1])
+def get_year_month_date(date_time: datetime, sep='.') -> str:
+    year_month_date = date_time.strftime(sep.join(['%Y', '%m']))
     return year_month_date
