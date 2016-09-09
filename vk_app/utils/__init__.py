b0761034378fcc5a3bc0400713f056d9b19c48d5
ace <azatibrakov@gmail.com>
2016-09-09 14:45:21 +0700

    Imports corrected

--- vk_app/utils/__init__.py
+++ vk_app/utils/__init__.py
@@ -1,2 +1,2 @@
 from .files import find_file, check_dir, get_valid_folders, download
-from .time import make_periodic, get_date_time_from_millis, get_year_month_date, DATE_FORMAT
+from .time import make_periodic, get_year_month_date
