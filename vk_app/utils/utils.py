7b2c1eaed4fbf9a83fbc3e6256cebd5915ec82b7
ace <azatibrakov@gmail.com>
2016-09-16 00:56:12 +0700

    Logging correcting

--- vk_app/utils/utils.py
+++ vk_app/utils/utils.py
@@ -9,6 +9,8 @@ __all__ = ['CallRepeater', 'CallDelayer', 'get_year_month_date', 'find_file', 'c
 
 VoidFunction = Callable[..., None]
 
+logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
@@ -50,7 +52,7 @@ class CallDelayer:
                 cls.last_call_time += delay_in_seconds
                 function(*args, **kwargs)
                 wait_sec = cls.last_call_time - time.time()
-                logging.info('Wait {} until next call'.format(wait_sec))
+                logger.info('Wait {} until next call'.format(wait_sec))
                 cls.call_event.wait(wait_sec)
 
             return launched_with_delay
@@ -61,7 +63,7 @@ class CallDelayer:
 @CallDelayer.make_delayed(1)
 def print_current_time():
     current_time = datetime.utcnow()
-    logging.info(current_time.strftime('%H:%M:%S:%f %d/%m/%Y'))
+    logger.info(current_time.strftime('%H:%M:%S:%f %d/%m/%Y'))
 
 
 def get_year_month_date(date_time: datetime, sep='.') -> str:
