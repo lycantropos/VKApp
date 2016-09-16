ac9db19f4237eeeecc2ec961821eac6839b6f44b
ace <azatibrakov@gmail.com>
2016-09-16 17:13:46 +0700

    Downloading corrected: 'urlopen' from 'None' url caused problems

--- vk_app/services/loading.py
+++ vk_app/services/loading.py
@@ -9,7 +9,7 @@ __MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS = 0.33
 
 @CallDelayer.make_delayed(__MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS)
 def download(link: str, save_path: str):
-    if not os.path.exists(save_path):
+    if not os.path.exists(save_path) and link:
         try:
             response = urlopen(link)
             if response.status == 200:
