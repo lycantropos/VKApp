9342e6ca62413eee54f68da1be1bcf0d914b70eb
ace <azatibrakov@gmail.com>
2016-09-19 22:33:08 +0700

    Logging corrected

--- vk_app/services/loading.py
+++ vk_app/services/loading.py
@@ -9,7 +9,7 @@ __MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS = 0.35
 
 @CallDelayer.make_delayed(__MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS)
 def download(url: str, save_path: str):
-    logging.debug("Loading to {} from {}".format(save_path, url))
+    logging.debug("Loading from {} to {}".format(url, save_path))
     if not os.path.exists(save_path) and url:
         try:
             response = urlopen(url)
@@ -18,5 +18,5 @@ def download(url: str, save_path: str):
                     image_content = response.read()
                     out.write(image_content)
         except OSError:
-            logging.exception("Can't download to {} from {}. Retrying...".format(save_path, url))
+            logging.exception("Can't download from {} to {}. Retrying...".format(url, save_path))
             download(url, save_path)
