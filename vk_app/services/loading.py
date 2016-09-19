2f5772a4d4db66ac34c281bf3441a1426df0076e
ace <azatibrakov@gmail.com>
2016-09-19 22:29:19 +0700

    Setting downloading delay to 0.35 sec, adding of download retrying if there's some conflict

--- vk_app/services/loading.py
+++ vk_app/services/loading.py
@@ -4,11 +4,12 @@ from urllib.request import urlopen
 
 from vk_app.utils import CallDelayer
 
-__MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS = 0.4
+__MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS = 0.35
 
 
 @CallDelayer.make_delayed(__MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS)
 def download(url: str, save_path: str):
+    logging.debug("Loading to {} from {}".format(save_path, url))
     if not os.path.exists(save_path) and url:
         try:
             response = urlopen(url)
@@ -16,6 +17,6 @@ def download(url: str, save_path: str):
                 with open(save_path, 'wb') as out:
                     image_content = response.read()
                     out.write(image_content)
-        except (ValueError, OSError):
-            error_description = "Can't download to {} from {}".format(save_path, url)
-            logging.exception(error_description)
+        except OSError:
+            logging.exception("Can't download to {} from {}. Retrying...".format(save_path, url))
+            download(url, save_path)
