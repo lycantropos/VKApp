aed62150e402f2e0b7a2eeb8f3d36bebf9702e2f
ace <azatibrakov@gmail.com>
2016-09-26 23:37:57 +0700

    Added 'VKPhoto' and 'VKAudio' classes to 'models' module

--- vk_app/models.py
+++ vk_app/models.py
@@ -1,8 +1,10 @@
 import os
 import shutil
+from datetime import datetime, time, timedelta
 from typing import List
 
-from vk_app.utils import find_file, check_dir
+from services.loading import download
+from vk_app.utils import find_file, check_dir, get_year_month_date, get_valid_dirs
 
 
 class VKObject:
@@ -43,7 +45,7 @@ class VKObject:
         """Must be overridden by inheritors"""
 
     @classmethod
-    def name(cls) -> str:
+    def attachment_key(cls) -> str:
         """
         For elements of attachments (such as VK photo, audio objects) should return their key in attachment object
         e.g. for VK photo object should return 'photo', for VK audio object should return 'audio' and etc.
@@ -52,3 +54,165 @@ class VKObject:
     @classmethod
     def from_raw(cls, raw_vk_object: dict) -> type:
         """Must be overridden by inheritors"""
+
+
+VK_ID_FORMAT = '{}_{}'
+
+
+class VKPhoto(VKObject):
+    def __init__(self, owner_id: int, photo_id: int, user_id: int, album: str, comment: str, date_time: datetime,
+                 link: str):
+        # VK utility fields
+        self.vk_id = VK_ID_FORMAT.format(owner_id, photo_id)
+        self.owner_id = owner_id
+        self.photo_id = photo_id
+        self.user_id = user_id
+
+        # info fields
+        self.album = album
+        self.comment = comment
+
+        # technical info fields
+        self.date_time = date_time
+        self.link = link
+
+    def __repr__(self):
+        return "<Photo(album='{}', link='{}', date_time='{}')>".format(
+            self.album, self.link, self.date_time
+        )
+
+    def __str__(self):
+        return "Photo from '{}' album".format(self.album)
+
+    @classmethod
+    def attachment_key(cls) -> str:
+        return 'photo'
+
+    def download(self, path: str):
+        image_subdirs = self.get_file_subdirs()
+        check_dir(path, *image_subdirs)
+
+        image_dir = os.path.join(path, *image_subdirs)
+        image_name = self.get_file_name()
+        image_path = os.path.join(image_dir, image_name)
+
+        download(self.link, image_path)
+
+    def get_file_subdirs(self) -> list:
+        year_month_date = get_year_month_date(self.date_time)
+        image_subdirs = get_valid_dirs(self.album, year_month_date)
+        return image_subdirs
+
+    def get_file_name(self) -> str:
+        image_name = self.link.split('/')[-1]
+        return image_name
+
+    def get_image_content(self, images_path: str, marked=True) -> bytearray:
+        image_path = self.get_file_path(images_path)
+        if marked:
+            image_path = image_path.replace('.jpg', '.png')
+
+        with open(image_path, 'rb') as marked_image:
+            image_content = marked_image.read()
+
+        return image_content
+
+    @classmethod
+    def from_raw(cls, raw_photo: dict) -> VKObject:
+        return cls(
+            int(raw_photo['owner_id']),
+            int(raw_photo['id']),
+            int(raw_photo.get('user_id', 0)),
+            raw_photo['album'],
+            raw_photo['text'],
+            datetime.fromtimestamp(raw_photo['date']),
+            cls.get_link(raw_photo)
+        )
+
+    @staticmethod
+    def get_link(raw_photo: dict) -> str:
+        photo_link_key_prefix = 'photo_'
+
+        photo_link_keys = list(
+            raw_photo_key
+            for raw_photo_key in raw_photo
+            if photo_link_key_prefix in raw_photo_key
+        )
+        photo_link_keys.sort(key=lambda x: int(x.replace(photo_link_key_prefix, '')))
+
+        highest_res_link_key = photo_link_keys[-1]
+        highest_res_link = raw_photo[highest_res_link_key]
+
+        return highest_res_link
+
+
+MAX_FILE_NAME_LEN = os.pathconf(os.getcwd(), 'PC_NAME_MAX')
+
+
+class Audio(VKObject):
+    FILE_NAME_FORMAT = "{artist} - {title}"
+    FILE_EXTENSION = ".mp3"
+
+    def __init__(self, owner_id: int, audio_id: int, artist: str, title: str, genre_id: int, lyrics_id: int,
+                 duration: time, date_time: datetime, link: str):
+        # VK utility fields
+        self.vk_id = VK_ID_FORMAT.format(owner_id, audio_id)
+        self.owner_id = owner_id
+        self.audio_id = audio_id
+
+        # info fields
+        self.artist = artist
+        self.title = title
+        self.genre_id = genre_id
+        self.lyrics_id = lyrics_id
+
+        # technical info fields
+        self.duration = duration
+        self.date_time = date_time
+        self.link = link
+
+    def __repr__(self):
+        return "<Audio(artist='{}', title='{}', duration='{}')>".format(
+            self.artist, self.title, self.duration
+        )
+
+    def __str__(self):
+        return "Audio called '{}'".format(
+            Audio.FILE_NAME_FORMAT.format(**self.__dict__)
+        )
+
+    @classmethod
+    def attachment_key(cls):
+        return 'audio'
+
+    def download(self, path: str):
+        audio_file_subdirs = self.get_file_subdirs()
+        check_dir(path, *audio_file_subdirs)
+
+        audio_file_dir = os.path.join(path, *audio_file_subdirs)
+        audio_file_name = self.get_file_name()
+        audio_file_path = os.path.join(audio_file_dir, audio_file_name)
+
+        download(self.link, audio_file_path)
+
+    def get_file_subdirs(self) -> str:
+        audio_file_subdirs = [self.artist]
+        return audio_file_subdirs
+
+    def get_file_name(self) -> str:
+        file_name = Audio.FILE_NAME_FORMAT.format(
+            **self.__dict__
+        )[:MAX_FILE_NAME_LEN - len(Audio.FILE_EXTENSION)].replace(os.sep, ' ') + Audio.FILE_EXTENSION
+        return file_name
+
+    @classmethod
+    def from_raw(cls, raw_vk_object: dict) -> VKObject:
+        return Audio(owner_id=int(raw_vk_object['owner_id']), audio_id=int(raw_vk_object['id']),
+                     artist=raw_vk_object['artist'].strip(), title=raw_vk_object['title'].strip(),
+                     genre_id=int(raw_vk_object.pop('genre_id', 0)), lyrics_id=int(raw_vk_object.pop('lyrics_id', 0)),
+                     duration=(
+                         datetime.min + timedelta(
+                             seconds=int(raw_vk_object['duration'])
+                         )
+                     ).time(), date_time=datetime.fromtimestamp(raw_vk_object['date']),
+                     link=raw_vk_object['url'] or None)
--- vk_app/services/vk_objects.py
+++ vk_app/services/vk_objects.py
@@ -2,19 +2,11 @@ from typing import List
 
 from vk_app.models import VKObject
 
-__all__ = ['get_vk_objects_from_raw', 'get_raw_vk_objects_from_posts']
-
-
-def get_vk_objects_from_raw(vk_object_cls: VKObject, raw_vk_objects: List[dict]) -> List[VKObject]:
-    vk_objects = list(
-        vk_object_cls.from_raw(raw_vk_object)
-        for raw_vk_object in raw_vk_objects
-    )
-    return vk_objects
+__all__ = ['get_raw_vk_objects_from_posts']
 
 
 def get_raw_vk_objects_from_posts(vk_object_cls: VKObject, posts: List[dict]) -> List[dict]:
-    vk_object_name = vk_object_cls.name()
+    vk_object_name = vk_object_cls.attachment_key()
     raw_vk_objects = list(
         attachment[vk_object_name]
         for post in posts
