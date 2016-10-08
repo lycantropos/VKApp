314ab5fa4b3ddb4d716829ca877507bcd7865805
ace <azatibrakov@gmail.com>
2016-10-08 10:51:18 +0700

    Removed extra decorator for making 'download' function delayed

--- vk_app/services/loading.py
+++ vk_app/services/loading.py
@@ -2,12 +2,7 @@ import logging
 import os
 from urllib.request import urlopen
 
-from vk_app.utils import CallDelayer
 
-__MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS = 0.35
-
-
-@CallDelayer.make_delayed(__MINIMAL_INTERVAL_BETWEEN_REQUESTS_IN_SECONDS)
 def download(url: str, save_path: str):
     logging.debug("Loading from {} to {}".format(url, save_path))
     if not os.path.exists(save_path) and url:
