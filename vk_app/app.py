5f22c5f2b17b0693f12508d9da7c166c66fd3dea
ace <azatibrakov@gmail.com>
2016-10-10 22:33:27 +0700

    Added methods for uploading files

--- vk_app/app.py
+++ vk_app/app.py
@@ -1,12 +1,14 @@
 import json
+from typing import List, Tuple
 
+import requests
 from vk import API, Session, AuthSession
 
 
 class App:
     def __init__(self, app_id: int = 0, user_login: str = '', user_password: str = '', scope: str = '',
                  access_token: str = '', api_version: str = '5.57'):
-        """Creates instance of our application for working with VK API.
+        """Initializes instance of our application for working with VK API.
         You have to specify authentication data for app (`app_id`) and user (`user_login`, `user_password`, `scope`)
          or `access_token` parameter.
 
@@ -41,7 +43,7 @@ class App:
         self.api_session = API(self.session, v=self.api_version)
 
     def get_items(self, method: str, **params):
-        """Get VK countable objects (wall posts, audios, photo albums, photos, videos, etc.)
+        """Returns VK countable objects (wall posts, audios, photo albums, photos, videos, etc.)
 
         :param method: name of API method. Ex.: 'photos.get'
 
@@ -51,7 +53,6 @@ class App:
         to get saved photos from 300 to 1300 of user with id 11283070 in chronological order
 
         more info about `method_name` parameters at https://vk.com/dev/`method_name`
-        :type params: dict
         :return:
         """
         params['count'] = 100
@@ -68,6 +69,42 @@ class App:
             offset = code_res['offset']
             if offset >= code_res['count']:
                 return items
+
+    def get_upload_server_url(self, method: str, **params) -> str:
+        """Returns VK server URL for uploading files on it
+
+        :param method: name of API method used to get upload server URL. Ex.: 'photos.getUploadServer'
+
+        for the full list check https://new.vk.com/dev/methods
+        :param params: method's parameters. Ex. for method 'photos.getWallUploadServer':
+        {}
+        to get upload server URL for images to be posted on current user's wall
+        :return:
+        """
+        response = self.api_session.__call__(method, **params)
+        upload_url = response['upload_url']
+        return upload_url
+
+    def upload_files_on_vk_server(self, method: str, upload_url: str,
+                                  files: List[Tuple[str, Tuple[str, bytearray]]], **params) -> List[dict]:
+        """Uploads files on VK servers and returns the list of raw VK objects
+
+        :param method: name of API method used to save given by VK IDs objects on user/community page.
+        Ex.: `photos.saveOwnerPhoto`.
+
+        for the full list check https://new.vk.com/dev/methods
+        :param upload_url: upload server URL which was gotten by `get_upload_server_url` method
+        :param files: tuples of 'file' strings with index number postfix and tuples of files' names with its content
+        :param params: method's parameters. Ex. for method 'audio.save':
+        {}
+        to get raw VK audio object with `artist` and `title` fields obtained from ID3 tags
+        """
+        with requests.Session() as session:
+            response = session.post(upload_url, files=files)
+            params.update(response.json())
+
+        raw_vk_objects = self.api_session.__call__(method, **params)
+        return raw_vk_objects
 
 
 VK_SCRIPT_GET_ALL = """var params = {params};
